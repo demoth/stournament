a12394e3d5eba6d36256870b949837ee0ee33137
demoth <bubnov.d.e@gmail.com>
2021-11-14 13:13:09 +0100

    add game controller
    serve state

---
+++ src/main/kotlin/org/davnokodery/stournament/GameController.kt
@@ -0,0 +1,24 @@
+package org.davnokodery.stournament
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class GameController {
+    val gameState = GameState()
+
+    @GetMapping("/state")
+    fun getState(): GameState {
+        return gameState;
+    }
+
+    @PostMapping("/action")
+    fun postAction(
+        @RequestParam("player") player: Int,
+        @RequestParam("card") cardId: String
+    ) {
+        TODO("WHATAFAAAAAAAAA BOOOOOOM")
+    }
+}
