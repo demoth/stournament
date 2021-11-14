f786deb9063da2b3773990612dfb5fc85e38d011
demoth <bubnov.d.e@gmail.com>
2021-11-14 14:05:37 +0100

    CORS

--- src/main/kotlin/org/davnokodery/stournament/GameController.kt
+++ src/main/kotlin/org/davnokodery/stournament/GameController.kt
@@ -1,10 +1,8 @@
 package org.davnokodery.stournament
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
+@CrossOrigin
 @RestController
 class GameController {
     val gameState = GameState()
