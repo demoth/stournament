a382ce279965c81dfaa01bc64f7a11e10c61c464
demoth <bubnov.d.e@gmail.com>
2022-04-24 23:39:04 +0200

    refactor test client
    hide messaging logic into test client functions

--- src/test/kotlin/org/davnokodery/rigel/IntegrationTest.kt
+++ src/test/kotlin/org/davnokodery/rigel/IntegrationTest.kt
@@ -7,7 +7,6 @@ import kotlinx.coroutines.runBlocking
 import org.davnokodery.rigel.TestDataCreator.Companion.testUser1
 import org.davnokodery.rigel.model.GameSessionStatus
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory
 import org.springframework.boot.test.context.SpringBootTest
@@ -29,52 +28,63 @@ class IntegrationTest {
     private val logger = LoggerFactory.getLogger(IntegrationTest::class.java)
     private val mapper = jacksonObjectMapper()
 
+    @Test
+    fun `start game - not enough players`() = runBlocking {
+        val testClient = TestClient(loginPost(testUser1))
+        testClient.login()
+        testClient.createGame()
+        testClient.startGame()
+        assertEquals("Not enough players", testClient.messages.firstOrNull()?.message)
+    }
 
     private val url
         get() = "localhost:$port"
 
-    @Test
-    fun `start game - not enough players`() = runBlocking {
+    private fun loginPost(user: User): String {
         val loginResponse = restTemplate.postForEntity<LoginResponse>(
             "http://$url/login",
-            LoginRequest(testUser1.name, testUser1.password)
+            LoginRequest(user.name, user.password)
         )
-        assertEquals(HttpStatus.OK, loginResponse.statusCode)
-
-        val handler = TestClient(loginResponse.body!!.jwt)
-        val session = StandardWebSocketClient().doHandshake(handler, "ws://$url/web-socket").get()!!
-
-        delay(200)
-        session.sendMessage(toJson(CreateGameMessage()))
-
-        delay(200)
-        assertNotNull(handler.newGameId, "Game is not created")
-
-        session.sendMessage(toJson(StartGameMessage()))
-        delay(200)
-        assertEquals("Not enough players", handler.messages.firstOrNull()?.message)
+        assertEquals(HttpStatus.OK, loginResponse.statusCode, "Could not login")
+        return loginResponse.body!!.jwt
     }
 
     private fun toJson(msg: ClientWsMessage) = TextMessage(mapper.writeValueAsString(msg))
 
-    inner class TestClient(val jwt: String) : WebSocketHandler {
+    inner class TestClient(private val jwt: String) : WebSocketHandler {
         private val mapper = jacksonObjectMapper()
+        private val session = StandardWebSocketClient().doHandshake(this, "ws://$url/web-socket").get()!!
 
         var newGameId: String? = null
         var gameStatus: GameSessionStatus? = null
 
         val messages = arrayListOf<GameMessageUpdate>()
 
+        suspend fun login() {
+            session.sendMessage(TextMessage(mapper.writeValueAsString(JwtMessage("Bearer $jwt"))))
+            delay(100)
+        }
+
+        suspend fun createGame() {
+            session.sendMessage(toJson(CreateGameMessage()))
+            delay(100)
+        }
+
+        suspend fun startGame() {
+            session.sendMessage(toJson(StartGameMessage()))
+            delay(100)
+        }
+
         override fun afterConnectionEstablished(session: WebSocketSession) {
             logger.debug("Connection established")
-            session.sendMessage(TextMessage(mapper.writeValueAsString(JwtMessage("Bearer $jwt"))))
-
         }
 
         override fun handleMessage(session: WebSocketSession, message: WebSocketMessage<*>) {
             logger.debug("Received message: $message")
-            if (message !is TextMessage)
+            if (message !is TextMessage) {
+                logger.error("Non text message received!")
                 return
+            }
 
             val msg: ServerWsMessage = mapper.readValue(message.payload)
             logger.debug("Received: $msg")
@@ -103,8 +113,6 @@ class IntegrationTest {
             logger.debug("connection closed status=$closeStatus")
         }
 
-        override fun supportsPartialMessages(): Boolean {
-            return false
-        }
+        override fun supportsPartialMessages() = false
     }
 }
