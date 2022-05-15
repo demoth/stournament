aed0762e0a2ad2eef1f06a4369885667381279cd
Filipp <samoylovfp@gmail.com>
2022-05-15 22:59:51 +0300

    WIP functional game list

--- front-end/src/app.rs
+++ front-end/src/app.rs
@@ -1,12 +1,17 @@
-use std::rc::Rc;
-
-use crate::{game_list::GameList, login::Login, server_api::ServerApi};
+use crate::{
+    game_list::GameList,
+    login::Login,
+    server_api::{RigelServerMessage, ServerApi},
+};
 use futures::lock::Mutex;
+use log::*;
+use std::rc::Rc;
 use yew::prelude::*;
+use yew_hooks::{use_list, UseListHandle};
 use yew_router::prelude::*;
 
 #[derive(Clone, Routable, PartialEq)]
-enum Route {
+pub enum Route {
     #[at("/")]
     Home,
     #[at("/login")]
@@ -21,12 +26,13 @@ enum Route {
 #[derive(Clone)]
 pub struct ApiContext {
     pub api: Rc<Mutex<ServerApi>>,
+    pub games_list: UseListHandle<String>
 }
 
 // The context is singleton so it is equal to itself
 impl PartialEq for ApiContext {
-    fn eq(&self, _other: &Self) -> bool {
-        true
+    fn eq(&self, other: &Self) -> bool {
+        self.games_list == other.games_list
     }
 }
 
@@ -40,9 +46,25 @@ fn switch(route: &Route) -> Html {
 
 #[function_component(App)]
 pub fn app() -> Html {
-    let onmessage = Callback::from(|m| {});
+    // FIXME: changing this does not notify the game_list component
+    let games_list = use_list::<String>(vec![]);
+
+    let onmessage = {
+        let games_list = games_list.clone();
+        Callback::from(move |m| {
+            info!("Server says {m:?}");
+            match m {
+                RigelServerMessage::GameStatusUpdate { newStatus } => {
+                    info!("New status {newStatus:?}")
+                }
+                RigelServerMessage::GamesListResponse { games } => games_list.set(games),
+                RigelServerMessage::NewGameCreated { gameId } => games_list.push(gameId),
+            };
+        })
+    };
     let api = ApiContext {
         api: Rc::new(Mutex::new(ServerApi::new(onmessage))),
+        games_list,
     };
     html! {
         <ContextProvider<ApiContext> context = {api}>
--- front-end/src/game_list.rs
+++ front-end/src/game_list.rs
@@ -1,6 +1,30 @@
+use wasm_bindgen_futures::spawn_local;
 use yew::prelude::*;
+use yew_hooks::use_list;
+
+use crate::app::ApiContext;
 
 #[function_component(GameList)]
 pub fn game_list() -> Html {
-    html! { {"Games"} }
+    let ctx = use_context::<ApiContext>().unwrap();
+    let api = ctx.api.clone();
+    spawn_local(async move { ctx.api.lock().await.list_games() });
+    let games = ctx.games_list;
+
+    html! {
+        <>
+        <button onclick={Callback::once(move |_|spawn_local(async move {
+            api.lock().await.new_game()
+        }))}> { "New game" } </button><br />
+        if games.current().is_empty() {
+            {"No games"}
+        } else {
+            <ul> {
+                for games.current().iter().map(|g| html!{
+                    <li> {g} </li>
+                })
+            } </ul>
+        }
+        </>
+    }
 }
